ce0bd388b45b264aa766dfeb602fdd87255d43ae
sys41x4 <cybersec.arijitbhowmick@gmail.com>
2022-03-08 00:16:04 +0530

    Added backend request handler for sending POST message and moved data upload code from "set_wallet" to "send_msg"

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 import subprocess
 # https://blog.logrocket.com/web3-py-tutorial-guide-ethereum-blockchain-development-with-python/
 from flask import Flask, jsonify, render_template, request, url_for, redirect, flash, session
+# import flask_socketio
 #from werkzeug.security import check_password_hash
 #from db import check_login, get_products, add_order_data, get_orders
 
@@ -41,6 +42,52 @@ def home(): ## func name changed from `set_walle` to `home`
     return render_template('wallet.html')
 
 
+@app.route('/message',methods=['GET']) # Added function for sending message
+def message():
+    return render_template('send_message.html')
+
+@app.route('/send_msg',methods= ['POST'])
+def send_msg():
+    sender = request.form['sender']
+    receiver = request.form['receiver']
+    message = request.form['message']
+    output = sender + ' --> ' + receiver + ' | ' + message
+    print({'sender':sender, 'receiver':receiver, 'message':message})
+    if sender and receiver and message:
+
+        Txn_Hash = 'Txn-Hash' # test Txn Hash
+
+        block_data = {
+        "From" : sender,
+        "To" : receiver,
+        "Block" : '<Previous Block Number>+1',
+        "Time" : time.time(),
+        "Txn-Hash" : 'New_Hash(<Previous Txn-Hash>+<New File Hash>)',
+        "Comment" : message,
+        "Result" : 'Success'
+        }
+
+        with open(Txn_Hash, 'w') as test_file:
+            test_file.write(json.dumps(block_data))
+
+        # os.popen('node put-files.js --token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGExMjU1MURGNUMxNzZmNDU0Y2EwRjQ1NUE0NUFjMjg4ODgzRjIwYzMiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NDYwNzU4NjY2NzQsIm5hbWUiOiJpcmMtdG9rZW4xIn0.sF0bUr8lwfr1e9-Yuv6-wJun1vP0JvKnR61sq7rMaTc test_data.json').read()
+    
+        ## Upload Conversation File ##
+        Upload_file = subprocess.Popen(["node", "put-files.js", "--token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGExMjU1MURGNUMxNzZmNDU0Y2EwRjQ1NUE0NUFjMjg4ODgzRjIwYzMiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NDYwNzU4NjY2NzQsIm5hbWUiOiJpcmMtdG9rZW4xIn0.sF0bUr8lwfr1e9-Yuv6-wJun1vP0JvKnR61sq7rMaTc", Txn_Hash], stdout=subprocess.PIPE, shell=True)
+        (CID_byte, err) = Upload_file.communicate()
+        CID = CID_byte.decode().replace('\n','').split(": ")[1]
+        # Get the uploaded File -> https://<CID>+.ipfs.dweb.link/<FileName>
+        print("CID Output => ", CID)
+
+        # Command Used to get the Txn-Hash Content
+        print(f'curl https://{CID}.ipfs.dweb.link/{Txn_Hash}')
+        # File to get https://<CID>+.ipfs.dweb.link/<Txn-Hash>
+
+        
+        return jsonify({'output': output})
+    return jsonify({'error' : 'Missing data!'})
+
+
 @app.route('/api/set_wallet',methods=['POST']) #from @app.get to @app.route
 def set_wallet_session():
     data = request.get_json()
@@ -49,36 +96,36 @@ def set_wallet_session():
     session['wallet_address'] = wallet_address
     print("Current MetaMask Wallet Address => ", wallet_address)
 
-    # Test Data
+    # # Test Data
     
-    test_json_data = {
-        "From" : wallet_address,
-        "To" : 'sys41x4',
-        "Block" : '<Previous Block Number>+1',
-        "Time" : time.time(),
-        "Txn-Hash" : 'New_Hash(<Previous Txn-Hash>+<New File Hash>)',
-        "Comment" : f'Hello Arijit Bhowmick it is {time.ctime()} in INDIA',
-        "Result" : 'Success'
-    }
+    # test_json_data = {
+    #     "From" : wallet_address,
+    #     "To" : 'sys41x4',
+    #     "Block" : '<Previous Block Number>+1',
+    #     "Time" : time.time(),
+    #     "Txn-Hash" : 'New_Hash(<Previous Txn-Hash>+<New File Hash>)',
+    #     "Comment" : f'Hello Arijit Bhowmick it is {time.ctime()} in INDIA',
+    #     "Result" : 'Success'
+    # }
     
-    Txn_Hash = 'Txn-Hash' # test Txn Hash
+    # Txn_Hash = 'Txn-Hash' # test Txn Hash
     
-    with open(Txn_Hash, 'w') as test_file:
-        test_file.write(json.dumps(test_json_data))
+    # with open(Txn_Hash, 'w') as test_file:
+    #     test_file.write(json.dumps(test_json_data))
 
 
-    # os.popen('node put-files.js --token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGExMjU1MURGNUMxNzZmNDU0Y2EwRjQ1NUE0NUFjMjg4ODgzRjIwYzMiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NDYwNzU4NjY2NzQsIm5hbWUiOiJpcmMtdG9rZW4xIn0.sF0bUr8lwfr1e9-Yuv6-wJun1vP0JvKnR61sq7rMaTc test_data.json').read()
+    # # os.popen('node put-files.js --token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGExMjU1MURGNUMxNzZmNDU0Y2EwRjQ1NUE0NUFjMjg4ODgzRjIwYzMiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NDYwNzU4NjY2NzQsIm5hbWUiOiJpcmMtdG9rZW4xIn0.sF0bUr8lwfr1e9-Yuv6-wJun1vP0JvKnR61sq7rMaTc test_data.json').read()
     
-    ## Upload Conversation File ##
-    Upload_file = subprocess.Popen(["node", "put-files.js", "--token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGExMjU1MURGNUMxNzZmNDU0Y2EwRjQ1NUE0NUFjMjg4ODgzRjIwYzMiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NDYwNzU4NjY2NzQsIm5hbWUiOiJpcmMtdG9rZW4xIn0.sF0bUr8lwfr1e9-Yuv6-wJun1vP0JvKnR61sq7rMaTc", Txn_Hash], stdout=subprocess.PIPE, shell=True)
-    (CID_byte, err) = Upload_file.communicate()
-    CID = CID_byte.decode().replace('\n','').split(": ")[1]
-    # Get the uploaded File -> https://<CID>+.ipfs.dweb.link/<FileName>
-    print("CID Output => ", CID)
+    # ## Upload Conversation File ##
+    # Upload_file = subprocess.Popen(["node", "put-files.js", "--token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGExMjU1MURGNUMxNzZmNDU0Y2EwRjQ1NUE0NUFjMjg4ODgzRjIwYzMiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NDYwNzU4NjY2NzQsIm5hbWUiOiJpcmMtdG9rZW4xIn0.sF0bUr8lwfr1e9-Yuv6-wJun1vP0JvKnR61sq7rMaTc", Txn_Hash], stdout=subprocess.PIPE, shell=True)
+    # (CID_byte, err) = Upload_file.communicate()
+    # CID = CID_byte.decode().replace('\n','').split(": ")[1]
+    # # Get the uploaded File -> https://<CID>+.ipfs.dweb.link/<FileName>
+    # print("CID Output => ", CID)
 
-    # Command Used to get the Txn-Hash Content
-    print(f'curl https://{CID}.ipfs.dweb.link/{Txn_Hash}')
-    # File to get https://<CID>+.ipfs.dweb.link/<Txn-Hash>
+    # # Command Used to get the Txn-Hash Content
+    # print(f'curl https://{CID}.ipfs.dweb.link/{Txn_Hash}')
+    # # File to get https://<CID>+.ipfs.dweb.link/<Txn-Hash>
     return 'OK'
 
 
