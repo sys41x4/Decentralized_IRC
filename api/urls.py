9751c060d42ddbe4dae29962bf4fabf2fa185130
Arijit Bhowmick <66935336+sys41x4@users.noreply.github.com>
2022-04-14 01:45:37 +0530

    Configured django settings with reffered to previous flask settings

---
+++ api/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("send_msg", views.send_msg, name="send_msg"),
+    path("set_wallet", views.set_wallet_session, name="set_wallet_session"),
+]
--- client_side/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from client_side import views
-
-urlpatterns = [
-    path('hello', views.client_side, name='client_side'),
-]
