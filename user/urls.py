853002cc47c8211773b82fe6db22fdf545c1ba00
Arijit Bhowmick <66935336+sys41x4@users.noreply.github.com>
2022-04-14 11:24:16 +0530

    remove gitignore files

--- user/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("message", views.message_page, name="message_page"),
-    path("account", views.user_account, name="user_account"),
-]
