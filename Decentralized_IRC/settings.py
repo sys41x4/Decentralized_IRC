80d16f2071739869dbf12c8fe189a265a8327043
Arijit Bhowmick <66935336+sys41x4@users.noreply.github.com>
2022-04-14 11:18:04 +0530

    Migrated flask based application to django

--- Decentralized_IRC/settings.py
+++ Decentralized_IRC/settings.py
@@ -122,6 +122,10 @@ STATICFILES_DIRS = [
    os.path.join(BASE_DIR, "static"),
    ]
 
+
+STATICFILES_FINDERS = [
+    'npm.finders.NpmFinder',
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
